4f757b7471568e8fd5d8f66222290d6999b05a87
ningosi <nicotwendelee@gmail.com>
2023-02-08 12:20:21 +0300

    Saving patient search based on a given identifier such that we only have one row per patient

--- omod/src/main/java/org/openmrs/module/initialpatientqueueapp/fragment/controller/QueuePatientFragmentController.java
+++ omod/src/main/java/org/openmrs/module/initialpatientqueueapp/fragment/controller/QueuePatientFragmentController.java
@@ -27,6 +27,7 @@ import org.openmrs.api.VisitService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.ehrcashier.billcalculator.BillCalculatorForBDService;
 import org.openmrs.module.ehrconfigs.metadata.EhrCommonMetadata;
+import org.openmrs.module.ehrconfigs.utils.EhrConfigsUtils;
 import org.openmrs.module.hospitalcore.BillingService;
 import org.openmrs.module.hospitalcore.HospitalCoreService;
 import org.openmrs.module.hospitalcore.PatientDashboardService;
@@ -700,15 +701,15 @@ public class QueuePatientFragmentController {
 	
 	private void savePatientSearch(Patient patient) {
 		HospitalCoreService hospitalCoreService = Context.getService(HospitalCoreService.class);
-		PatientSearch patientSearch = new PatientSearch();
-		String givenName = "";
-		String fullname = "";
-		String middleName = "";
-		String familyName = "";
-		Timestamp birtDate = null;
-		PatientIdentifier patientIdentifier;
+		
 		if (patient != null && hospitalCoreService.getPatientByPatientId(patient.getPatientId()) == null) {
 			//log.error("Starting with patient>>" + patient.getPatientId());
+			PatientSearch patientSearch = new PatientSearch();
+			String givenName = "";
+			String fullname = "";
+			String middleName = "";
+			String familyName = "";
+			Timestamp birtDate = null;
 			givenName = patient.getGivenName();
 			familyName = patient.getFamilyName();
 			if (patient.getMiddleName() != null) {
@@ -716,24 +717,20 @@ public class QueuePatientFragmentController {
 			}
 			fullname = givenName + " " + middleName + " " + familyName;
 			birtDate = new Timestamp(patient.getBirthdate().getTime());
-			patientIdentifier = patient.getPatientIdentifier();
-			
-			if (patientIdentifier != null) {
-				patientSearch.setPatientId(patient.getPatientId());
-				patientSearch.setIdentifier(patientIdentifier.getIdentifier());
-				patientSearch.setFullname(fullname);
-				patientSearch.setGivenName(givenName);
-				patientSearch.setMiddleName(middleName);
-				patientSearch.setFamilyName(familyName);
-				patientSearch.setGender(patient.getGender());
-				patientSearch.setBirthdate(birtDate);
-				patientSearch.setAge(patient.getAge());
-				patientSearch.setPersonNameId(patient.getPersonName().getPersonNameId());
-				patientSearch.setDead(false);
-				patientSearch.setAdmitted(false);
-				//commit the patient object in the patient_search table
-				hospitalCoreService.savePatientSearch(patientSearch);
-			}
+			patientSearch.setPatientId(patient.getPatientId());
+			patientSearch.setIdentifier(EhrConfigsUtils.getPreferredPatientIdentifier(patient));
+			patientSearch.setFullname(fullname);
+			patientSearch.setGivenName(givenName);
+			patientSearch.setMiddleName(middleName);
+			patientSearch.setFamilyName(familyName);
+			patientSearch.setGender(patient.getGender());
+			patientSearch.setBirthdate(birtDate);
+			patientSearch.setAge(patient.getAge());
+			patientSearch.setPersonNameId(patient.getPersonName().getPersonNameId());
+			patientSearch.setDead(false);
+			patientSearch.setAdmitted(false);
+			//commit the patient object in the patient_search table
+			hospitalCoreService.savePatientSearch(patientSearch);
 			
 		}
 	}
