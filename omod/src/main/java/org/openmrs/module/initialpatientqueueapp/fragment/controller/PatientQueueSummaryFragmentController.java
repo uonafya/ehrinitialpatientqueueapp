e62c1893ddbd90db620523f420a092ce7cb70b46
agesa3 <collinsagesa2000@gmail.com>
2023-07-28 09:59:01 +0300

    list patient queue summary

--- omod/src/main/java/org/openmrs/module/initialpatientqueueapp/fragment/controller/PatientQueueSummaryFragmentController.java
+++ omod/src/main/java/org/openmrs/module/initialpatientqueueapp/fragment/controller/PatientQueueSummaryFragmentController.java
@@ -8,7 +8,15 @@ import org.openmrs.module.ehrconfigs.metadata.EhrCommonMetadata;
 import org.openmrs.parameter.EncounterSearchCriteria;
 import org.openmrs.ui.framework.fragment.FragmentModel;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.openmrs.module.kenyaemr.api.KenyaEmrService;
+import org.openmrs.ui.framework.SimpleObject;
+import org.openmrs.ui.framework.UiUtils;
+import org.openmrs.ui.framework.fragment.FragmentConfiguration;
+import org.openmrs.ui.framework.fragment.FragmentModel;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.openmrs.module.patientdashboardapp.model.VisitDetail;
 
+import javax.tools.SimpleJavaFileObject;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -23,12 +31,17 @@ public class PatientQueueSummaryFragmentController {
 		            encounterService.getEncounterTypeByUuid(EhrCommonMetadata._EhrEncounterTypes.REGINITIAL),
 		            encounterService.getEncounterTypeByUuid(EhrCommonMetadata._EhrEncounterTypes.REGREVISIT)), null, null,
 		        null, false));
+		
 		if (!patientEncounters.isEmpty() && patientEncounters.size() > 5) {
-			//filtered = //get the first 5
+			//order by date then filter the first 5
+			filtered = patientEncounters.subList(0, 5);
 		} else {
 			filtered = patientEncounters;
 			
 		}
 		model.addAttribute("encounters", filtered);
+		model.addAttribute("patient", patient);
+		model.addAttribute("userLocation", Context.getService(KenyaEmrService.class).getDefaultLocation().getName());
 	}
+	
 }
